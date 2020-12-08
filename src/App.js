f42df178059258f8bbd6bfccdc0d15e303910caf
Marthe J. Neverdal <mjneverdal@outlook.com>
2020-12-08 14:34:31 +0100

    added font awesome. made css to css modules.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import AppStyles from './App.module.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { } from '@fortawesome/free-bold-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className={AppStyles.App}>
+      <Button variant="danger">Danger</Button>
+      <FontAwesomeIcon icon={faFacebook} />
+    </div>
+  );
 };
 
 export default App;
